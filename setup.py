2dbc8b37319abc42cadb732b3a10e3285d7388df
Michael Klier <chi@chimeric.de>
2009-01-20 00:25:18 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup
+
+setup(
+    # metadata for upload to PyPI
+    author = "Michael Klier",
+    author_email = "chi@chimeric.de",
+    description = "DokuWikiXMLRPC provides methods to access DokuWiki instances via the XML-RPC protocol.",
+    license = "GNU GPL-2",
+    keywords = "dokuwiki xmlrpc",
+    url = "http://chimeric.de/projects/dokuwikixmlrpc",
+
+    # project data
+    name = "dokuwikixmlrpc",
+    version = "0.1",
+    py_modules = ['dokuwikixmlrpc']
+)
+
+# vim:ts=4:sw=4:et:enc=utf-8:
