42d1e3e788953160f20c1dd141c323647537b96e
Florian Rathgeber <florian.rathgeber@gmail.com>
2021-03-28 23:19:05 +0200

    Fix formatting

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     long_description = f.read()
 
 setup(
-    name = "dokuwikixmlrpc",
-    version = "2020.5.23",
+    name="dokuwikixmlrpc",
+    version="2020.5.23",
     description="DokuWiki XML-RPC module.",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    py_modules = ["dokuwikixmlrpc"],
-    author = "Michael Klier",
-    author_email = "chi@chimeric.de",
-    url = "https://github.com/kynan/dokuwikixmlrpc",
-    download_url = "https://github.com/downloads/kynan/dokuwikixmlrpc/dokuwikixmlrpc.tgz",
-    keyword = ["dokuwiki", "xmlrpc", "xml-rpc"],
+    long_description_content_type="text/x-rst",
+    py_modules=["dokuwikixmlrpc"],
+    author="Michael Klier",
+    author_email="chi@chimeric.de",
+    url="https://github.com/kynan/dokuwikixmlrpc",
+    download_url="https://github.com/downloads/kynan/dokuwikixmlrpc/dokuwikixmlrpc.tgz",
+    keyword=["dokuwiki", "xmlrpc", "xml-rpc"],
     license="License :: OSI Approved :: MIT License",
-    classifiers = [
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
